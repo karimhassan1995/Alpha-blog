ed044603c6b8f8610ef16ae2f03af848b9da7e1d
karimhassan1995 <karimelmorsy95@gmail.com>
2022-10-04 18:50:39 +0200

    Add users table to the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_27_224323) do
+ActiveRecord::Schema.define(version: 2022_10_04_133611) do
 
   create_table "articles", force: :cascade do |t|
     t.string "name"
     t.string "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
